0370daaeef56d3706948b332b8c4b03aaed344ea
USEINOV RASIM <r.usieinov@gmail.com>
2023-02-07 00:47:18 +0300

    destroy like routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
-  resources :rooms, only: %i[show create]#, param: :title
-  resources :messages, only: %i[create update edit destroy] do
-    member { post :like }
-  end
+  resources :rooms, only: %i[show create destroy]#, param: :title
+  resources :messages, only: %i[create update edit destroy]
   root "rooms#index"
 end
